6d3e02d56785ba73ff13ab17a27a41e0fcfba5f5
X <1377556122@qq.com>
2016-04-03 23:05:01 +0800

    Added files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import random
+import os
+youya=[u'天下', u'杀伐', u'人家', u'韶华', u'风华', u'繁华', u'血染', u'墨染', u'白衣', u'素衣', u'嫁衣', u'倾城', u'孤城', u'空城', u'旧城', u'旧人', u'伊人', u'心疼', u'春风', u'古琴', u'凄凉', u'黄梁', u'未央', u'成双', u'无恙', u'虚妄', u'凝霜', u'洛阳', u'长安', u'江南', u'忘川', u'千年', u'纸伞', u'烟雨', u'回眸', u'公子', u'红尘', u'红颜', u'红衣', u'红豆', u'红线', u'青丝', u'青史', u'青冢', u'白发', u'白首', u'白骨', u'黄土', u'黄泉', u'碧落', u'紫陌 ']
+keke=[ u'梨花', u'落花', u'烟花', u'离殇', u'情殇', u'爱殇', u'剑殇', u'灼伤', u'仓皇', u'匆忙', u'陌上', u'清商', u'焚香', u'墨香', u'微凉', u'断肠', u'无情', u'迷离', u'奈何', u'断弦', u'焚尽', u'散乱', u'陌路', u'乱世', u'笑靥', u'浅笑', u'明眸', u'轻叹', u'烟火', u'一生', u'三生', u'浮生', u'桃花', u'痴狂']
+hehe=[u'情深缘浅', u'情深不寿', u'莫失莫忘', u'阴阳相隔', u'如花美眷', u'似水流年' , u'眉目如画',u'曲终人散' , u'繁华落尽' , u'不诉离殇' , u'一世长安']
+
+print youya[random.randint(0,len(youya)-1)],youya[random.randint(0,len(youya)-1)],',',youya[random.randint(0,len(youya)-1)],u'了',hehe[random.randint(0,len(hehe)-1)],';',youya[random.randint(0,len(youya)-1)],youya[random.randint(0,len(youya)-1)],',',youya[random.randint(0,len(youya)-1)],u'了',hehe[random.randint(0,len(hehe)-1)],'.'
+print hehe[random.randint(0,len(hehe)-1)],hehe[random.randint(0,len(hehe)-1)],u'不过是',hehe[random.randint(0,len(hehe)-1)],'.',u'你的',youya[random.randint(0,len(youya)-1)],u'我的',youya[random.randint(0,len(youya)-1)],',',u'最后不过', hehe[random.randint(0,len(hehe)-1)],'.'
+print youya[random.randint(0,len(youya)-1)],youya[random.randint(0,len(youya)-1)],',',u'最后不过',hehe[random.randint(0,len(hehe)-1)],'.'
+print youya[random.randint(0,len(youya)-1)],youya[random.randint(0,len(youya)-1)],',',u'终究不敌',hehe[random.randint(0,len(hehe)-1)],'.'
